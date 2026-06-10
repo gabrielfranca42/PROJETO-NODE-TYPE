cd8e6850a27ed377a28f0febed924dca14d9796e
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-06-10 17:15:55 -0300

    adição de eslint ao projeto

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
